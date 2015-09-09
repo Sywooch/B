4e59c5865fc493c686687a332407b420a5107327
余云建 <yuyj@mysoft.com.cn>
2015-09-09 10:35:07 +0800

    reset
    重新初始化

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -1,9 +1,63 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
+        'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+        'modules'    => [
+                'user' => [
+                        'class'                    => 'dektrium\user\Module',
+                        'mailer'                   => [
+                                'sender'                => 'no-reply@myhost.com',
+                                // or ['no-reply@myhost.com' => 'Sender name']
+                                'welcomeSubject'        => 'Welcome subject',
+                                'confirmationSubject'   => 'Confirmation subject',
+                                'reconfirmationSubject' => 'Email change subject',
+                                'recoverySubject'       => 'Recovery subject',
+
+                        ],
+                        'enableUnconfirmedLogin'   => true,
+                        'confirmWithin'            => 86400,
+                        'cost'                     => 12,
+                        'enableGeneratingPassword' => false,#自动生成密码，并通过邮件发送
+                        'enableConfirmation'       => true, #开启邮件确认
+                        'enableUnconfirmedLogin'   => true, #未认证用户是否可以登陆
+                        'rememberFor'              => 1209600, #cookie有效期，2周
+                        'admins'                   => ['admin'],#管理员账号
+
+                ],
+                'rbac' => [
+                        'class' => 'dektrium\rbac\Module',
+                ],
         ],
-    ],
+        'components' => [
+                'cache'   => [
+                        'class' => 'yii\caching\FileCache',
+                ],
+                'session' => [
+                        'class'        => 'yii\web\Session',
+                        'timeout'      => 7200,
+                        'name'         => 'PHPSESSID',
+                        'cookieParams' => [
+                                'domain'   => '.yii2.com',
+                                'httponly' => true,
+                                'path'     => '/admin',
+                        ],
+
+                ],
+                'mailer'  => [
+                        'class'            => 'yii\swiftmailer\Mailer',
+                        'useFileTransport' => false,//这句一定有，false发送邮件，true只是生成邮件在runtime文件夹下，不发邮件
+                        'transport'        => [
+                                'class'      => 'Swift_SmtpTransport',
+                                'host'       => 'smtp.163.com',
+                                'username'   => '15618380091@163.com',
+                                'password'   => '*******',
+                                'port'       => '25',
+                                'encryption' => 'tls',
+
+                        ],
+                        'messageConfig'    => [
+                                'charset' => 'UTF-8',
+                                'from'    => ['admin@qq.com' => 'admin']
+                        ],
+                ],
+        ],
 ];
--- advanced/frontend/config/main.php
+++ advanced/frontend/config/main.php
@@ -1,33 +1,46 @@
 <?php
-$params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
-);
+$params = array_merge(require(__DIR__ . '/../../common/config/params.php'),
+        require(__DIR__ . '/../../common/config/params-local.php'), require(__DIR__ . '/params.php'),
+        require(__DIR__ . '/params-local.php'));
 
 return [
-    'id' => 'app-frontend',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'frontend\controllers',
-    'components' => [
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+        'id'                  => 'app-frontend',
+        'basePath'            => dirname(__DIR__),
+        'bootstrap'           => ['log'],
+        'controllerNamespace' => 'frontend\controllers',
+        'modules'             => [
+                'user' => [
+                    // following line will restrict access to admin page
+                    'as frontend' => 'dektrium\user\filters\FrontendFilter',
                 ],
-            ],
         ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
+        'components'          => [
+                'user'         => [
+                        'identityCookie' => [
+                                'name'     => '_frontendIdentity',
+                                'path'     => '/',
+                                'httpOnly' => true,
+                        ],
+                ],
+                'session'      => [
+                        //'name'         => 'FRONTENDSESSID',
+                        'cookieParams' => [
+                                'httpOnly' => true,
+                                'path'     => '/',
+                        ],
+                ],
+                'log'          => [
+                        'traceLevel' => YII_DEBUG ? 3 : 0,
+                        'targets'    => [
+                                [
+                                        'class'  => 'yii\log\FileTarget',
+                                        'levels' => ['error', 'warning'],
+                                ],
+                        ],
+                ],
+                'errorHandler' => [
+                        'errorAction' => 'site/error',
+                ],
         ],
-    ],
-    'params' => $params,
+        'params'              => $params,
 ];
